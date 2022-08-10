2f5858f91c6d706c691d7b5c4dca7a440c4e9347
Bianca Ribeiro <biancaribeiro93036@gmail.com>
2022-08-10 13:23:21 +0000

    Adicionado HTML na rota contatos.

--- app.py
+++ app.py
@@ -8,4 +8,16 @@ def hello():
 
 @app.route("/contatos")
 def contato():
-    return "Bibi"
+    return """
+    <html>
+        <head>
+            <title> Contatos </title>
+        </head>
+
+        <body>
+            <h1>Bibi, e-mail</h1>
+            <h2>bibibubu@hotmail.com</h2>
+            <h3>15 555 5555 555</h3>
+        </body>
+    </html>
+    """
